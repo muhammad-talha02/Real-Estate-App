d2a0a972603e3dc9b5c9d697386cabd18f69dbc6
Muhammad Talha Saeed <iamtalha971@gmail.com>
2025-02-25 22:14:52 +0400

    feat: setup database on appwrite

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,4 +1,4 @@
-import { Account, Avatars, Client, OAuthProvider } from "react-native-appwrite";
+import { Account, Avatars, Client, Databases, OAuthProvider } from "react-native-appwrite";
 import * as WebBrowser from "expo-web-browser";
 
 import * as Linking from "expo-linking";
@@ -7,6 +7,11 @@ export const config = {
   platform: "com.cc.realestateapp",
   endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
   projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+  databaseId: process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID,
+  agentsCollectionId: process.env.EXPO_PUBLIC_APPWRITE_AGENTS_COLLECTION_ID,
+  galleriesCollectionId: process.env.EXPO_PUBLIC_APPWRITE_GALLERIES_COLLECTION_ID,
+  reviewsCollectionId: process.env.EXPO_PUBLIC_APPWRITE_REVIEWS_COLLECTION_ID,
+  propertiesCollectionId: process.env.EXPO_PUBLIC_APPWRITE_PROPERTIES_COLLECTION_ID,
 };
 
 export const client = new Client();
@@ -18,7 +23,7 @@ client
 
 export const avatar = new Avatars(client);
 export const account = new Account(client);
-
+export const databases = new Databases(client)
 export async function login() {
   try {
     const redirectUri = Linking.createURL("/");
